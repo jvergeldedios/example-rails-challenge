b37bd635c766e3e62ad27009c9784d20430358bb
Julian Vergel de Dios <Julian@Julians-MacBook-Pro.local>
2014-01-25 02:05:54 -0800

    charge and customer cleanup work

--- source/app/models/charge.rb
+++ source/app/models/charge.rb
@@ -1,3 +1,20 @@
 class Charge < ActiveRecord::Base
-  
+  module Currency
+    USD = 'usd'
+    EUR = 'eur'
+  end
+
+  CURRENCIES = [Currency::USD, Currency::EUR]
+
+  validates_presence_of :amount
+  validates_presence_of :currency
+  validates_inclusion_of :currency, in: CURRENCIES
+
+  belongs_to :customer
+
+  scope :optimized, -> { includes(:user) }
+  scope :successful, -> { where(failed_on: nil, disputed_on: nil) }
+  scope :failed, -> { where('failed_on IS NOT NULL') }
+  scope :disputed, -> { where('disputed_on IS NOT NULL') }
+
 end
--- source/app/models/customer.rb
+++ source/app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ActiveRecord::Base
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
+  has_many :charges
+
 end
